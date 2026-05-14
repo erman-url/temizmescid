525714613ca082551084d248ae7ed61181c388fc
Erman Urel <erman.urel@gmail.com>
2026-05-14 10:08:42 +0300

    asd

---
+++ footer.js
@@ -0,0 +1,67 @@
+document.write(`
+
+<nav class="footer">
+
+<div
+class="nav-item"
+onclick="location.href='index.html'"
+>
+
+<span class="nav-icon">⌂</span>
+
+<p>
+Ana
+</p>
+
+</div>
+
+<div
+class="nav-item"
+onclick="location.href='mescidler.html'"
+>
+
+<span class="nav-icon">★</span>
+
+<p>
+Mescidler
+</p>
+
+</div>
+
+<button
+type="button"
+class="center-btn"
+onclick="location.href='index.html'"
+>
++
+</button>
+
+<div
+class="nav-item"
+onclick="location.href='sosyal.html'"
+>
+
+<span class="nav-icon">◎</span>
+
+<p>
+Sosyal
+</p>
+
+</div>
+
+<div
+class="nav-item"
+onclick="location.href='iletisim.html'"
+>
+
+<span class="nav-icon">✉</span>
+
+<p>
+İletişim
+</p>
+
+</div>
+
+</nav>
+
+`);
