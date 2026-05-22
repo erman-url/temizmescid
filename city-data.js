ad69ca74898e019e98cbc7691fbbab56bd2caa9c
Erman Urel <erman.urel@gmail.com>
2026-05-22 11:36:23 +0300

    Deploy v1.0.102

--- city-data.js
+++ city-data.js
@@ -1,32 +1,305 @@
 window.CITY_DATA = {
 
-"İstanbul":[
-"Adalar",
-"Arnavutköy",
-"Ataşehir",
-"Avcılar",
-"Bahçelievler",
-"Bakırköy",
-"Başakşehir",
-"Bayrampaşa",
-"Beşiktaş",
-"Beylikdüzü",
-"Beyoğlu",
-"Kadıköy",
-"Küçükçekmece"
-],
+  "Adana":[
+    "Aladağ",
+    "Ceyhan",
+    "Çukurova",
+    "Feke",
+    "İmamoğlu",
+    "Karaisalı",
+    "Karataş",
+    "Kozan",
+    "Pozantı",
+    "Saimbeyli",
+    "Sarıçam",
+    "Seyhan",
+    "Tufanbeyli",
+    "Yumurtalık",
+    "Yüreğir"
+  ],
 
-"Ankara":[
-"Çankaya",
-"Keçiören",
-"Mamak",
-"Yenimahalle"
-],
+  "Adıyaman":[
+    "Besni",
+    "Çelikhan",
+    "Gerger",
+    "Gölbaşı",
+    "Kahta",
+    "Merkez",
+    "Samsat",
+    "Sincik",
+    "Tut"
+  ],
 
-"İzmir":[
-"Bornova",
-"Karşıyaka",
-"Konak"
-]
+  "Afyonkarahisar":[
+    "Başmakçı",
+    "Bayat",
+    "Bolvadin",
+    "Çay",
+    "Çobanlar",
+    "Dazkırı",
+    "Dinar",
+    "Emirdağ",
+    "Evciler",
+    "Hocalar",
+    "İhsaniye",
+    "İscehisar",
+    "Kızılören",
+    "Merkez",
+    "Sandıklı",
+    "Sinanpaşa",
+    "Sultandağı",
+    "Şuhut"
+  ],
+
+  "Ağrı":[
+    "Diyadin",
+    "Doğubayazıt",
+    "Eleşkirt",
+    "Hamur",
+    "Merkez",
+    "Patnos",
+    "Taşlıçay",
+    "Tutak"
+  ],
+
+  "Amasya":[
+    "Göynücek",
+    "Gümüşhacıköy",
+    "Hamamözü",
+    "Merkez",
+    "Merzifon",
+    "Suluova",
+    "Taşova"
+  ],
+
+  "Ankara":[
+    "Akyurt",
+    "Altındağ",
+    "Ayaş",
+    "Bala",
+    "Beypazarı",
+    "Çamlıdere",
+    "Çankaya",
+    "Çubuk",
+    "Elmadağ",
+    "Etimesgut",
+    "Evren",
+    "Gölbaşı",
+    "Güdül",
+    "Haymana",
+    "Kahramankazan",
+    "Kalecik",
+    "Keçiören",
+    "Kızılcahamam",
+    "Mamak",
+    "Nallıhan",
+    "Polatlı",
+    "Pursaklar",
+    "Sincan",
+    "Şereflikoçhisar",
+    "Yenimahalle"
+  ],
+
+  "Antalya":[
+    "Akseki",
+    "Aksu",
+    "Alanya",
+    "Demre",
+    "Döşemealtı",
+    "Elmalı",
+    "Finike",
+    "Gazipaşa",
+    "Gündoğmuş",
+    "İbradı",
+    "Kaş",
+    "Kemer",
+    "Kepez",
+    "Konyaaltı",
+    "Korkuteli",
+    "Kumluca",
+    "Manavgat",
+    "Muratpaşa",
+    "Serik"
+  ],
+
+  "Artvin":[
+    "Ardanuç",
+    "Arhavi",
+    "Borçka",
+    "Hopa",
+    "Kemalpaşa",
+    "Merkez",
+    "Murgul",
+    "Şavşat",
+    "Yusufeli"
+  ],
+
+  "Aydın":[
+    "Bozdoğan",
+    "Buharkent",
+    "Çine",
+    "Didim",
+    "Efeler",
+    "Germencik",
+    "İncirliova",
+    "Karacasu",
+    "Karpuzlu",
+    "Koçarlı",
+    "Köşk",
+    "Kuşadası",
+    "Kuyucak",
+    "Nazilli",
+    "Söke",
+    "Sultanhisar",
+    "Yenipazar"
+  ],
+
+  "Balıkesir":[
+    "Altıeylül",
+    "Ayvalık",
+    "Balya",
+    "Bandırma",
+    "Bigadiç",
+    "Burhaniye",
+    "Dursunbey",
+    "Edremit",
+    "Erdek",
+    "Gömeç",
+    "Gönen",
+    "Havran",
+    "İvrindi",
+    "Karesi",
+    "Kepsut",
+    "Manyas",
+    "Marmara",
+    "Savaştepe",
+    "Sındırgı",
+    "Susurluk"
+  ],
+
+  "Bilecik":[
+    "Bozüyük",
+    "Gölpazarı",
+    "İnhisar",
+    "Merkez",
+    "Osmaneli",
+    "Pazaryeri",
+    "Söğüt",
+    "Yenipazar"
+  ],
+
+  "Bingöl":[
+    "Adaklı",
+    "Genç",
+    "Karlıova",
+    "Kiğı",
+    "Merkez",
+    "Solhan",
+    "Yayladere",
+    "Yedisu"
+  ],
+
+  "Bitlis":[
+    "Adilcevaz",
+    "Ahlat",
+    "Güroymak",
+    "Hizan",
+    "Merkez",
+    "Mutki",
+    "Tatvan"
+  ],
+
+  "Bolu":[
+    "Dörtdivan",
+    "Gerede",
+    "Göynük",
+    "Kıbrıscık",
+    "Mengen",
+    "Merkez",
+    "Mudurnu",
+    "Seben",
+    "Yeniçağa"
+  ],
+
+  "Burdur":[
+    "Ağlasun",
+    "Altınyayla",
+    "Bucak",
+    "Çavdır",
+    "Çeltikçi",
+    "Gölhisar",
+    "Karamanlı",
+    "Kemer",
+    "Merkez",
+    "Tefenni",
+    "Yeşilova"
+  ],
+
+  "Bursa":[
+    "Büyükorhan",
+    "Gemlik",
+    "Gürsu",
+    "Harmancık",
+    "İnegöl",
+    "İznik",
+    "Karacabey",
+    "Keles",
+    "Kestel",
+    "Mudanya",
+    "Mustafakemalpaşa",
+    "Nilüfer",
+    "Orhaneli",
+    "Orhangazi",
+    "Osmangazi",
+    "Yenişehir",
+    "Yıldırım"
+  ],
+
+  "Çanakkale":[
+    "Ayvacık",
+    "Bayramiç",
+    "Biga",
+    "Bozcaada",
+    "Çan",
+    "Eceabat",
+    "Ezine",
+    "Gelibolu",
+    "Gökçeada",
+    "Lapseki",
+    "Merkez",
+    "Yenice"
+  ],
+
+  "Çankırı":[
+    "Atkaracalar",
+    "Bayramören",
+    "Çerkeş",
+    "Eldivan",
+    "Ilgaz",
+    "Kızılırmak",
+    "Korgun",
+    "Kurşunlu",
+    "Merkez",
+    "Orta",
+    "Şabanözü",
+    "Yapraklı"
+  ],
+
+  "Çorum":[
+    "Alaca",
+    "Bayat",
+    "Boğazkale",
+    "Dodurga",
+    "İskilip",
+    "Kargı",
+    "Laçin",
+    "Mecitözü",
+    "Merkez",
+    "Oğuzlar",
+    "Ortaköy",
+    "Osmancık",
+    "Sungurlu",
+    "Uğurludağ"
+  ]
 
 };
